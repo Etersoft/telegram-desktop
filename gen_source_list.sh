3e4b3801e4d0c65bf06b73b48982d82fd6be318f
Vitaly Lipatov <lav@etersoft.ru>
2019-07-07 00:54:14 +0300

    - update spec to new build 1.7.13 (with rpmrb script) - use external librlottie instead of internal qtlottie

--- gen_source_list.sh
+++ gen_source_list.sh
@@ -12,10 +12,12 @@ grep "^<(src_loc)" Telegram/gyp/telegram_sources.txt | \
     grep -v "platform/mac" | grep -v "platform/win" | \
     sed -e "s|<(src_loc)|    SourceFiles|g" >> $OFILE
 
-grep "<(src_loc)" Telegram/gyp/lib_lottie.gyp | \
-    grep -v "platform/mac" | grep -v "platform/win" | \
-    grep -v "pch_" | grep "\." | \
-    sed -e "s|.*<(src_loc)\(.*\)',.*|    SourceFiles\1|g" >> $OFILE
+for i in lottie ffmpeg ; do
+    grep "<(src_loc)" Telegram/gyp/lib_$i.gyp | \
+        grep -v "platform/mac" | grep -v "platform/win" | \
+        grep -v "pch_" | grep "\." | \
+        sed -e "s|.*<(src_loc)\(.*\)',.*|    SourceFiles\1|g" >> $OFILE
+done
 
 cat <<EOF >>$OFILE
 )
